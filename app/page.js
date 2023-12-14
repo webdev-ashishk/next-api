05250d21738d20d3d6fe880c3a3cc510f6e2462e
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-14 14:59:05 +0530

    added frontend

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 async function getData() {
   const res = await fetch(process.env.API_URL);
+  console.log(process.env.API_URL);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -13,6 +14,7 @@ export default async function Home() {
   return (
     <div style={{ margin: "10px" }}>
       <h1> this is backend of next-JS </h1>
+      <h3>Welcome to frontend of Nextjs</h3>
       <h1>List of all users!!</h1>
       {data.map((items) => {
         return (
